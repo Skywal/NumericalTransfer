8e7243b24c4e793226d190bb4cf5682f6e6e47ef
Andriy Derckach <skaywoker@gmail.com>
2017-05-24 17:17:18 +0300

    button click listeners configured

--- app/src/main/java/derkach/andrei/numericaltransfer/MainScreen.java
+++ app/src/main/java/derkach/andrei/numericaltransfer/MainScreen.java
@@ -2,8 +2,10 @@ package derkach.andrei.numericaltransfer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+    }
+    /**обробник натисенння кнопки СТАРТ*/
+    public void startButtonPush(View view){
+        // для перевірки роботи
+        Toast.makeText(this, "START button is pushed", Toast.LENGTH_SHORT).show();
+    }
+    /**Обробник натиснення кнопки ІФОРМАЦІЯ*/
+    public void informationButtonPush(View view){
+        //для перевірки роботи
+        Toast.makeText(this, "INFORMATION button is pushed", Toast.LENGTH_SHORT).show();
     }
 }
