8acb9ab63d0e65a0ecbc7f391194fa03bb7b1c38
Andriy Derckach <skaywoker@gmail.com>
2017-05-24 18:06:16 +0300

    New activities tied with buttons

--- app/src/main/java/derkach/andrei/numericaltransfer/MainScreen.java
+++ app/src/main/java/derkach/andrei/numericaltransfer/MainScreen.java
@@ -1,13 +1,14 @@
 package derkach.andrei.numericaltransfer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
+    /**Для виклику інших activity*/
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,16 @@ public class MainScreen extends AppCompatActivity {
     }
     /**обробник натисенння кнопки СТАРТ*/
     public void startButtonPush(View view){
-        // для перевірки роботи
-        Toast.makeText(this, "START button is pushed", Toast.LENGTH_SHORT).show();
+        /**Запускаємо новий activity*/
+        intent = new Intent(this, MainTrainingScreen.class); // контекст і клас який викликаємо
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY); // завершує роботу activity у фоні
+        startActivity(intent); // запуск
     }
     /**Обробник натиснення кнопки ІФОРМАЦІЯ*/
     public void informationButtonPush(View view){
-        //для перевірки роботи
-        Toast.makeText(this, "INFORMATION button is pushed", Toast.LENGTH_SHORT).show();
+        /**Запускаємо новий activity*/
+        intent = new Intent(this, InformationScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
     }
 }
