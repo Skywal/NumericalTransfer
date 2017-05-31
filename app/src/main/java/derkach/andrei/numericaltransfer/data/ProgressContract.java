7d2f4639725730e18d464d2d8c65e6f73c6b569b
Andriy Derckach <skaywoker@gmail.com>
2017-05-31 15:41:08 +0300

    add basic structure of contract class

---
+++ app/src/main/java/derkach/andrei/numericaltransfer/data/ProgressContract.java
@@ -0,0 +1,24 @@
+package derkach.andrei.numericaltransfer.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Bloom on 30.05.2017.
+ * клас контракт для роботи із базою даних
+ */
+
+public final class ProgressContract {
+    private ProgressContract(){
+    }
+    /**окремий клас для роботи із таблицею прогресу переведення із десятків
+     * у двійки*/
+    public static final class DecToBinProgress implements BaseColumns{
+        /*назва таблиці*/
+        public final static String TABLE_NAME = "dec_to_bin";
+
+        /*назва стовпців таблиці*/
+        public final static String _ID = BaseColumns._ID; // головне ключове поле
+        public final static String COLUMN_DECIMAL_NUM = "decimal_num"; // завдання
+        public final static String COLUMN_ANSWER_BIN = "answer_bin"; // відповідь
+    }
+}
--- app/src/main/java/derkach/andrei/numericaltransfer/data/ProgressContranct.java
+++
@@ -1,9 +0,0 @@
-package derkach.andrei.numericaltransfer.data;
-
-/**
- * Created by Bloom on 30.05.2017.
- * клас контракт для роботи із базою даних
- */
-
-public class ProgressContranct {
-}
