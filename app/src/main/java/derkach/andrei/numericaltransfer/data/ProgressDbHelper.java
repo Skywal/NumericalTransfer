22d929309e906b9e5dc02ff1e119c88d0160dfdd
Andriy Derckach <skaywoker@gmail.com>
2017-05-31 18:03:02 +0300

    add basic structure of ProgressDbHelper class

--- app/src/main/java/derkach/andrei/numericaltransfer/data/ProgressDbHelper.java
+++ app/src/main/java/derkach/andrei/numericaltransfer/data/ProgressDbHelper.java
@@ -6,18 +6,37 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by Bloom on 31.05.2017.
+ * допоміжний клас для роботи із базою даних
  */
 
 public class ProgressDbHelper extends SQLiteOpenHelper {
-    public ProgressDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    /**назва файлу бази даних*/
+    public static final String DATABASE_NAME = "progress.db";
+    /**версія бази даних
+     * якщо змінюється схема бази то збільшувати на один*/
+    public static final int DATABASE_VERSION = 1;
+
+    /**Конструктор*/
+    public ProgressDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**Викликається при створенні бази данних*/
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        /*рядок для створення таблиці*/
+        String SQL_CREATE_PROGRESS_TABLE = "create table "
+                + ProgressContract.DecToBinProgress.TABLE_NAME + " ("
+                + ProgressContract.DecToBinProgress._ID + " integer primary key autoincrement,"
+                + ProgressContract.DecToBinProgress.COLUMN_TASK_DEC + " integer not null,"
+                + ProgressContract.DecToBinProgress.COLUMN_ANSWER_BIN + " string not null);";
 
+        /*запускаємо створення таблиці*/
+        sqLiteDatabase.execSQL(SQL_CREATE_PROGRESS_TABLE);
     }
 
+    /**Викликається при обнові схеми бази данних*/
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
